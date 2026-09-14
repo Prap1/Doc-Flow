90e58c7e90f9d052283a6faa71e3c8bfa2a08f9b
Prap1 <praphoolk32@gmail.com>
2026-09-14 20:39:55 +0530

    refactor: move stylesheet imports to main.jsx and add favicon

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./store";
+
+// Vendor stylesheets
+import "react-toastify/dist/ReactToastify.css";
+import "react-calendar/dist/Calendar.css";
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
+
+// Application styles
 import "./index.css";
 import App from "./App.jsx";
 
